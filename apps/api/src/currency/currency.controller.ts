7fcda3e9d89c1d896f1c694b8addbb1b9e1d7cce
benacq <benacq44@gmail.com>
2024-01-12 15:24:09 +0000

    prisma configured

--- apps/api/src/currency/currency.controller.ts
+++ apps/api/src/currency/currency.controller.ts
@@ -1,38 +1,56 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { CurrencyService } from './currency.service';
 import { CreateCurrencyDto } from './dto/create-currency.dto';
 import { UpdateCurrencyDto } from './dto/update-currency.dto';
 import { CurrencyEntity } from './entities/currency.entity';
+import { CurrencyCode } from '@prisma/client';
 
 @Controller('currency')
 export class CurrencyController {
-  constructor(private readonly currencyService: CurrencyService) {}
+  constructor(private readonly currencyService: CurrencyService) { }
 
-  @Post()
-  create(@Body() createCurrencyDto: CreateCurrencyDto) {
-    return this.currencyService.create(createCurrencyDto);
-  }
-
-  @Get()
-  findAll() {
+  @Get("/rates")
+  getFxRate( @Query('from') from: string,  @Query('to') to: string ) {
     // this.currencyService.getFxRates(new CurrencyEntity(0, "GHS", "Ghana Cedi", "GHS"))
-    this.currencyService.getFxRate(new CurrencyEntity(0, "GHS", "Ghana Cedi", "GHS"), new CurrencyEntity(0, "EUR", "Ghana Cedi", "GHS"))
-
-    return this.currencyService.findAll();
+    if(to === undefined || to === null || to === ""){
+      return this.currencyService.getFxRates(from as CurrencyCode)
+    }
+    return this.currencyService.getFxRate(from as CurrencyCode, to as CurrencyCode)
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.currencyService.findOne(+id);
-  }
+  // @Get("/rates")
+  // getFxRates( @Query('from') from: string) {
+  //   return this.currencyService.getFxRates(from as CurrencyCode)
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCurrencyDto: UpdateCurrencyDto) {
-    return this.currencyService.update(+id, updateCurrencyDto);
-  }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.currencyService.remove(+id);
-  }
+  // @Post()
+  // create(@Body() createCurrencyDto: CreateCurrencyDto) {
+  //   return this.currencyService.create(createCurrencyDto);
+  // }
+
+  // @Get()
+  // findAll() {
+  //   // this.currencyService.getFxRates(new CurrencyEntity(0, "GHS", "Ghana Cedi", "GHS"))
+  //   this.currencyService.getFxRate(new CurrencyEntity(0, "GHS", "Ghana Cedi", "GHS"), new CurrencyEntity(0, "EUR", "Ghana Cedi", "GHS"))
+
+  //   return this.currencyService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.currencyService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateCurrencyDto: UpdateCurrencyDto) {
+  //   return this.currencyService.update(+id, updateCurrencyDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.currencyService.remove(+id);
+  // }
+
+
 }
--- apps/api/src/currency/currency.service.ts
+++ apps/api/src/currency/currency.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CurrencyRepository } from './currency.repository';
 import { UpdateCurrencyDto } from './dto/update-currency.dto';
 import { CreateCurrencyDto } from './dto/create-currency.dto';
-import { Currency } from '@prisma/client';
+import { Currency, CurrencyCode } from '@prisma/client';
 import { ExchangeRatesService } from 'src/exchange-rates/exchange-rates.service';
 
 
@@ -11,34 +11,12 @@ export class CurrencyService {
   constructor(private currencyRepo: CurrencyRepository, private exchangeService: ExchangeRatesService) { }
 
 
-  getFxRate(from:Currency, to: Currency){
-     this.exchangeService.getRate(from.code, to.code)
+  getFxRate(from:CurrencyCode, to: CurrencyCode){
+     return this.exchangeService.getRate(from, to)
   }
 
-  
-
-  getFxRates(from:Currency){
-    this.exchangeService.getRates(from.code)
+  getFxRates(from:CurrencyCode){
+    return this.exchangeService.getRates(from)
  }
 
-  create(createWalletDto: CreateCurrencyDto) {
-    return this.currencyRepo.create(createWalletDto);
-  }
-
-  findAll() {
-    return this.currencyRepo.findAll();
-  }
-
-  async findOne(id: number) {
-    const wallet = await this.currencyRepo.findOne(id);
-    return wallet;
-  }
-
-  update(id: number, updateWalletDto: UpdateCurrencyDto) {
-    return this.currencyRepo.update(id, updateWalletDto);
-  }
-
-  remove(id: number) {
-    return this.currencyRepo.remove(id);
-  }
 }
